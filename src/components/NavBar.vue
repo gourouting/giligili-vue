09a9b701e16e32030aad246ee38dfe0194e716ad
jam <european@iqing.com>
2019-06-29 12:22:19 +0800

    G站初始化

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-menu class="el-menu-demo" mode="horizontal" router>
+    <el-menu-item index="/">首页</el-menu-item>
+    <el-menu-item index="/about">关于我们</el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="">
+      Giligili世界大和平！
+    </div>
   </div>
 </template>
 
